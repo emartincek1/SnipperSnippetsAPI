272164999d3db4c819bc335e13d016bb69d1046e
emartincek1 <evan.martincek@gmail.com>
2024-04-11 12:42:01 -0500

    create snippet entity, set up connection string and make initial migration

---
+++ SnipperSnippetsAPI/SnipperSnippetsAPI/Data/SnipperSnippetsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnipperSnippetsAPI.Entities;
+
+namespace SnipperSnippetsAPI.Data
+{
+    public class SnipperSnippetsDbContext: DbContext
+    {
+        public SnipperSnippetsDbContext(DbContextOptions<SnipperSnippetsDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Snippet> Snippets { get; set; }
+    }
+}
---
+++ SnipperSnippetsAPI/SnipperSnippetsAPI/Entities/Snippet.cs
@@ -0,0 +1,9 @@
+﻿namespace SnipperSnippetsAPI.Entities
+{
+    public class Snippet
+    {
+        public int id { get; set; }
+        public string language { get; set; }
+        public string code { get; set; }
+    }
+}
